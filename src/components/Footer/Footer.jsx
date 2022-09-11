7e9d22b49d35608f7bdd897852b339d2f72b2ca5
EDCarlitosSchoolxD <uxdj050728hqrczna1@cbtis111.edu.mx>
2022-09-11 17:05:04 -0500

    Nombre del segundo integrante

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ export const Footer = () => {
   return (
     <footer>
         <h2>Hecho por Juan Carlos Uch Dzib &#169; el mas humilde de mexico</h2>
+        <h2>Erick Daniel Haidar Colly</h2>
     </footer>
 
     )
